a31c5023688f4b6c29f0b27633eab0d640fee227
Tsalexey <github.kalmar-1o1D>
2020-01-24 02:55:04 +0300

    commit task 4 first part

--- src/ru/rudn/science/belov_course/task4/task4.kt
+++ src/ru/rudn/science/belov_course/task4/task4.kt
@@ -1,29 +1,48 @@
 package ru.rudn.science.belov_course.task4
 
+import java.io.File
 import kotlin.math.pow
 
 fun main(args: Array<String>) {
-
+    val nodesCountList = arrayListOf<Int>(100, 200, 400, 800)
     val xRange = 0..10 // x stands for space
     val tRange = 0..10 // t stands for time
 
-    val N: Int // space nodes count
-    val M: Int // time nodes count
 
-    val nodesCountList = arrayListOf<Int>(100, 200, 400, 800)
+    for (nodes in nodesCountList) {
+        val N = nodes // space nodes count
+        val M = nodes // time nodes count
 
-    val nodesCount = 100
+        val h: Double = (xRange.last - xRange.first).toDouble() / N
+        val tau: Double = (tRange.last - tRange.first).toDouble() / M
 
-    N = nodesCount
-    M = nodesCount
+        var u = hashMapOf<Int, ArrayList<Double>>()
 
-    val stepSpace = (xRange.last - xRange.first) / N
-    val stepTime = (tRange.last - tRange.first) / M
+        var xInitlist = mutableListOf<Double>()
+        var tInitList = mutableListOf<Double>()
 
-    // u_time_init = x^2
-    // u_space_init = 0
-    
-    // u_time + u_space = 0
+        (0..N).forEach { xInitlist.add(it.toDouble().pow(2)) }
+        (0..M).forEach { tInitList.add(0.0)}
 
+        u.put(0, xInitlist.toMutableList() as ArrayList<Double>)
+        (1..M).forEach { u.put(it, tInitList.toMutableList() as ArrayList<Double>) }
 
+        for (n in 1..N) {
+            for (m in 1..M) {
+                val derrivative = ((tau / h) * u.get(n - 1)!!.get(m) + u.get(n)!!.get(m - 1)!!) / (1 + tau / h)
+                u.get(n)?.set(m, derrivative)
+            }
+        }
+
+        // todo: find estimation by richardson
+
+        val file = "task4_data/task4_u_${nodes}nodes.txt";
+        File(file).printWriter().use { out ->
+            for(n in 0..N){
+                for(m in 0..M){
+                    out.println("${0+h*n};${0+tau*m};${u.get(n)!!.get(m)!!}")
+                }
+            }
+        }
+    }
 }
