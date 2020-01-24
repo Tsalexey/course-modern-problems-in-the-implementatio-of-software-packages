a16dda6df8cb92185a884293109dcf4d17d94572
Alexey Tsarev <alexey.tsarev@netcracker.com>
2020-01-24 19:46:26 +0300

    commit gradle build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,8 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    kotlin("jvm").version("1.3.21")
+    kotlin("jvm") version "1.3.21"
 
-    // Apply the application plugin to add support for building a CLI application.
-    application
 }
 
 repositories {
@@ -20,15 +18,11 @@ repositories {
 
 dependencies {
     // Use the Kotlin JDK 8 standard library.
-    implementation.(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
 
     // Use the Kotlin test library.
     testImplementation(kotlin("test"))
 
     // Use the Kotlin JUnit integration.
     testImplementation(kotlin("test-junit"))
-}
-
-application {
-
 }
