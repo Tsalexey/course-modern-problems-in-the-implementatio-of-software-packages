ed133857d4f0d27256bd08668f29fc2d5f7f6e01
Tsalexey <github.kalmar-1o1D>
2020-01-25 22:33:19 +0300

    add task 6 (explicit)

--- src/ru/rudn/science/belov_course/task1/task6/task6.kt
+++ src/ru/rudn/science/belov_course/task1/task6/task6.kt
@@ -1,10 +1,13 @@
 package ru.rudn.science.belov_course.task1.task6
 
+import java.io.File
+import kotlin.math.abs
+import kotlin.math.exp
 import kotlin.math.pow
 
 fun main(args: Array<String>) {
-    val N = 10
-    val M = 10
+    val N = 100
+    val M = 100
 
     val xRange = 0..1
     val tRange = 0..1
@@ -12,34 +15,70 @@ fun main(args: Array<String>) {
     explicit(xRange, tRange, N, M)
 }
 
-fun explicit(x: IntRange, t: IntRange, N: Int, M: Int) {
+fun f(x: Double, t: Double): Double = exp(x * t)
 
-    val h: Double = ((x.last - x.first) / N).toDouble()
-    val tau: Double = ((t.last - t.first) / M).toDouble()
+fun point(range: IntRange, nodeNumber: Int, step: Double): Double = range.first.toDouble() + step * nodeNumber
+
+fun explicit(x: IntRange, t: IntRange, N: Int, M: Int) {
+    val h: Double = (x.last.toDouble() - x.first.toDouble()) / N
+    val tau: Double = (t.last.toDouble() - t.first.toDouble()) / M
 
     val u = hashMapOf<Int, MutableMap<Int, Double>>()
 
     // init u
-    (1 until N + 1).forEach { n -> u.put(n, HashMap()) }
+    (0..M).forEach { m -> u.put(m, HashMap()) }
 
-    (1 until N + 1).forEach { n ->
-        (1 until M + 1).forEach { m ->
-            when (n) {
-                1 -> (u.getValue(m).set(n, 1.0))
-                N -> (u.getValue(m).set(n, N.toDouble()))
-                else -> (u.getValue(m).set(n, 0.0))
-            }
-            if (n == 1) {
-                u.getValue(n).set(m, m.toDouble().pow(2))
+    (0..M).forEach { m ->
+        (0..N).forEach { n ->
+            (u.getValue(m).set(n, Double.NaN))
+            if (n == 0) (u.getValue(m).set(n, 0.0))
+            if (n == N) (u.getValue(m).set(n, 1.0))
+            if (m == 0) u.getValue(m).set(n, point(x, n, h).pow(2))
+        }
+    }
+
+    var xPoint: Double
+    var tPoint: Double
+    var phi: Double
+    var uCurent: Double
+    var uPrev: Double
+    var uNext: Double
+
+    (0..M - 1).forEach { m ->
+        (1..N - 1).forEach { n ->
+            xPoint = point(x, n, h)
+            tPoint = point(t, m, tau)
+            phi = f(xPoint, tPoint)
+            uCurent = u.getValue(m).getValue(n)
+            uPrev = u.getValue(m).getValue(n - 1)
+            uNext = u.getValue(m).getValue(n + 1)
+//            println("m=${m}, n=${n}, x=${xPoint}, t=${tPoint}, f=${phi}, uC=${uCurent}, uP=${uPrev}, uN=${uNext}")
+            u.getValue(m + 1).set(n, tau * phi + uCurent + (tau / h.pow(2)) * (uNext - 2 * uCurent + uPrev))
+        }
+    }
+
+    val file = "task6_data/task6_explicit.txt"
+    File(file).printWriter().use { out ->
+        (0..M).forEach { m ->
+            (0..N).forEach { n ->
+                out.println("${point(x, n, h)};${point(t, m, tau)};${u.getValue(m).getValue(n)}")
             }
         }
     }
 
-    for (i in 1..N) {
-        for (j in 1..M) {
-            print("${u.getValue(i).get(j)} ")
+    for (time in listOf<Double>(0.0, 0.25, 0.5, 0.75, 1.0)) {
+        val file = "task6_data/task6_explicit_time${time}.txt"
+        File(file).printWriter().use { out ->
+            File(file).printWriter().use { out ->
+                (0..M).forEach { m ->
+                    (0..N).forEach { n ->
+                        if (abs(point(x, m, tau) - time) < 0.00001) {
+                            out.println("${point(x, n, h)};${point(t, m, tau)};${u.getValue(m).getValue(n)}")
+                        }
+                    }
+                }
+            }
         }
-        println()
     }
 }
 
