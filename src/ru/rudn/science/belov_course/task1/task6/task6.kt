29b5bcce68a648f6f64de7ab3c95aacb875e910a
Tsalexey <github.kalmar-1o1D>
2020-01-26 00:20:10 +0300

    complete task6 done + add data

--- src/ru/rudn/science/belov_course/task1/task6/task6.kt
+++ src/ru/rudn/science/belov_course/task1/task6/task6.kt
@@ -66,32 +66,69 @@ fun explicit(x: IntRange, t: IntRange, N: Int, M: Int, times: List<Double>) {
     save(u, "explicit", 0..M, 0..N, x, h, t, tau, times)
 }
 
+fun calculateD(u: Map<Int, MutableMap<Int, Double>>, n: Int, m: Int, x: IntRange, t: IntRange, h: Double, tau: Double): Double {
+    return -tau * h.pow(2) * f(point(x, n, h), point(t, m - 1, tau)) - h.pow(2) * u.getValue(m - 1).getValue(n)
+}
+
 fun implicit(x: IntRange, t: IntRange, N: Int, M: Int, times: List<Double>) {
-//    val h: Double = (x.last.toDouble() - x.first.toDouble()) / N
-//    val tau: Double = (t.last.toDouble() - t.first.toDouble()) / M
-//
-//    val u = init(0..N, 0..M, N, h)
-//
-//    var xPoint: Double
-//    var tPoint: Double
-//    var phi: Double
-//    var uCurent: Double
-//    var uPrev: Double
-//    var uNext: Double
-//
-//    (0..M - 1).forEach { m ->
-//        (1..N - 1).forEach { n ->
-//            xPoint = point(x, n, h)
-//            tPoint = point(t, m, tau)
-//            phi = f(xPoint, tPoint)
-//            uCurent = u.getValue(m).getValue(n)
-//            uPrev = u.getValue(m).getValue(n - 1)
-//            uNext = u.getValue(m).getValue(n + 1)
-//            u.getValue(m + 1).set(n, tau * phi + uCurent + (tau / h.pow(2)) * (uNext - 2 * uCurent + uPrev))
-//        }
-//    }
-//
-//    save(u, "implicit", 0..M, 0..N, x, h, t, tau, times)
+    val h: Double = (x.last.toDouble() - x.first.toDouble()) / N
+    val tau: Double = (t.last.toDouble() - t.first.toDouble()) / M
+
+    val u = init(0..N, 0..N, N, h)
+
+    (1..M).forEach { m ->
+        var A: Double = tau
+        var B: Double = -(h.pow(2) + 2 * tau)
+        var C: Double = tau
+
+        val V = mutableMapOf<Int, Double>()
+        val D = mutableMapOf<Int, Double>()
+        val F = mutableMapOf<Int, Double>()
+
+        V.put(0, u.getValue(m).getValue(0))
+        V.put(N, u.getValue(m).getValue(N))
+
+        (1..N - 1).forEach { n ->
+            V.put(n, u.getValue(m).getValue(n))
+            D.put(n, calculateD(u, n, m, x, t, h, tau))
+            F.put(n, D.getValue(n))
+        }
+
+        F.set(1, F.getValue(1) - A * V.getValue(0))
+        F.set(N - 1, F.getValue(N - 1) - C * V.getValue(N))
+
+        val alpha = mutableMapOf<Int, Double>()
+        val beta = mutableMapOf<Int, Double>()
+
+        alpha.put(1, C / B)
+        beta.put(1, F.getValue(1) / B)
+
+        (2..N - 1).forEach { i ->
+            alpha.put(i, C / (B - A * alpha.getValue(i - 1)))
+            beta.put(i, (F.getValue(i) - A * beta.getValue(i - 1)) / (B - A * alpha.getValue(i - 1)))
+        }
+
+        V.put(N - 1, beta.getValue(N - 1))
+        for (i in N - 2 downTo 1) {
+            V.put(i, beta.getValue(i) - alpha.getValue(i) * V.getValue(i + 1))
+        }
+
+        (1..N - 1).forEach { i ->
+            u.getValue(m).put(i, V.getValue(i))
+        }
+
+    }
+
+    save(u, "implicit", 0..M, 0..N, x, h, t, tau, times)
+}
+
+fun debug(map: Map<Int, Double>, N: Int, s: String) {
+    println()
+    println(s)
+
+    (0..N).forEach { i ->
+        println("${i} : ${map.getOrDefault(i, Double.NaN)} ")
+    }
 }
 
 fun save(result: Map<Int, Map<Int, Double>>,
@@ -113,7 +150,7 @@ fun save(result: Map<Int, Map<Int, Double>>,
     }
 
     for (time in times) {
-        val file = "task6_data/task6_explicit_time${time}.txt"
+        val file = "task6_data/task6_${filename}_time${time}.txt"
         File(file).printWriter().use { out ->
             File(file).printWriter().use { out ->
                 (tRange).forEach { m ->
