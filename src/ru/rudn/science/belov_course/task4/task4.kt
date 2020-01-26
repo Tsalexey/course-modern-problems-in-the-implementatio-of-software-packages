2f868fa1f8992a31098ce92a39c4d3ab320507dc
Tsalexey <github.kalmar-1o1D>
2020-01-26 18:50:07 +0300

    refactor task 4

--- src/ru/rudn/science/belov_course/task4/task4.kt
+++ src/ru/rudn/science/belov_course/task4/task4.kt
@@ -1,21 +1,26 @@
 package ru.rudn.science.belov_course.task4
 
-import java.io.File
+import ru.rudn.science.belov_course.Utils
 import kotlin.math.pow
 
-fun main(args: Array<String>) {
+object Conditions {
     val nodesCountList = arrayListOf<Int>(100, 200, 400, 800)
-    val xRange = 0..10
-    val tRange = 0..10
+    val x = 0..10
+    val t = 0..10
 
-    for (nodes in nodesCountList) {
-        val N = nodes // space nodes count
-        val M = nodes // time nodes count
+    val times = arrayListOf(0.0, 2.5, 5.0, 7.5, 10.0)
+}
 
-        val h: Double = (xRange.last - xRange.first).toDouble() / N
-        val tau: Double = (tRange.last - tRange.first).toDouble() / M
+fun main(args: Array<String>) {
 
-        var u = init(0..N + 1, 0..M + 1)
+    for (nodes in Conditions.nodesCountList) {
+        val N = nodes
+        val M = nodes
+
+        val h: Double = (Conditions.x.last - Conditions.x.first).toDouble() / N
+        val tau: Double = (Conditions.t.last - Conditions.t.first).toDouble() / M
+
+        val u = Utils.init(0..N, 0..M, ::applyCondition)
 
         (0..M - 1).forEach { m ->
             (0..N - 1).forEach { n ->
@@ -24,32 +29,15 @@ fun main(args: Array<String>) {
                 u.getValue(m + 1).put(n + 1, (h * u_np1_m + tau * u_n_mp1) / (h + tau))
             }
         }
+        Utils.saveU(u, "task4_data", "task4_u_${nodes}", 0..N, 0..M, Conditions.x, h, Conditions.t, tau, Conditions.times)
 
         // todo: find estimation by richardson
-
-        val file = "task4_data/task4_u_${nodes}nodes.txt"
-        File(file).printWriter().use { out ->
-            for (n in 0..N) {
-                for (m in 0..M) {
-                    out.println("${0 + h * n};${0 + tau * m};${u.get(n)!!.get(m)}")
-                }
-            }
-        }
     }
 }
 
-
-fun init(x: IntRange, t: IntRange): Map<Int, MutableMap<Int, Double>> {
-    val u = hashMapOf<Int, MutableMap<Int, Double>>()
-
-    t.forEach { m -> u.put(m, HashMap()) }
-
-    t.forEach { m ->
-        x.forEach { n ->
-            u.getValue(m).set(n, Double.NaN)
-            if (n == 0) u.getValue(m).put(n, 0.0)
-            if (m == 0) u.getValue(m).put(n, n.toDouble().pow(2))
-        }
-    }
-    return u
+fun applyCondition(u: Map<Int, MutableMap<Int, Double>>, m: Int, n: Int) {
+    u.getValue(m).set(n, Double.NaN)
+    if (n == 0) u.getValue(m).put(n, 0.0)
+    if (m == 0) u.getValue(m).put(n, n.toDouble().pow(2))
+    return;
 }
