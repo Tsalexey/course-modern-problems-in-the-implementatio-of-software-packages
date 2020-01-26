7e9a8ed22ba52281bf55adde53ad2b5f900f46b4
Tsalexey <github.kalmar-1o1D>
2020-01-26 05:30:38 +0300

    refactor task5

--- src/ru/rudn/science/belov_course/task1/task5/task5.kt
+++ src/ru/rudn/science/belov_course/task1/task5/task5.kt
@@ -2,11 +2,12 @@ package ru.rudn.science.belov_course.task1.task5
 
 import java.io.File
 import java.math.BigDecimal
+import kotlin.math.abs
 import kotlin.math.pow
 
 fun main(args: Array<String>) {
     val N = 10
-    val M = 100
+    val M = 10
 
     val xRange = 0..10
     val tRange = 0..10
@@ -14,18 +15,20 @@ fun main(args: Array<String>) {
     val h: Double = (xRange.last - xRange.first).toDouble() / N
     val tau: Double = (tRange.last - tRange.first).toDouble() / M
 
+    println("h=${h}, tau=${tau}, R=${tau / h.pow(2)}")
+
     var u = init(0..N + 1, 0..M + 1)
 
     (0..M - 1).forEach { m ->
+        println("m=${m}/${M - 1}")
         (0..N - 1).forEach { n ->
             val u_np1_m = u.getValue(m).getValue(n + 1)
             val u_n_m = u.getValue(m).getValue(n)
-            val u_n_mp1 = u.getValue(m + 1).getValue(n)
-
-            u.getValue(m + 1).put(n + 1, (BigDecimal.valueOf(h) * u_np1_m + u_n_m * BigDecimal.valueOf(tau) * u_n_mp1) / (BigDecimal.valueOf(h) + u_n_m * BigDecimal.valueOf(tau)))
+            u.getValue(m + 1).put(n + 1, (BigDecimal.valueOf(h) * u_np1_m - u_n_m * BigDecimal.valueOf(tau) * u_np1_m + BigDecimal.valueOf(tau) * u_n_m.pow(2)) / BigDecimal.valueOf(h))
         }
     }
 
+    println("saving results")
     val file = "task5_data/task5_${N}_${M}.txt"
     File(file).printWriter().use { out ->
         (0..M).forEach { m ->
@@ -40,7 +43,7 @@ fun main(args: Array<String>) {
         File(file2).printWriter().use { out ->
             (0..M).forEach { m ->
                 (0..N).forEach { n ->
-                    if ((0 + tau * m) - t.toDouble() < 0.00001) out.println("${0 + h * n};${u.getValue(m).getValue(n)}")
+                    if (abs((0 + tau * m) - t.toDouble()) < 0.00001) out.println("${0 + h * n};${0 + tau * m};${u.getValue(m).getValue(n)}")
                 }
             }
         }
@@ -55,8 +58,8 @@ fun init(x: IntRange, t: IntRange): Map<Int, MutableMap<Int, BigDecimal>> {
     t.forEach { m ->
         x.forEach { n ->
             u.getValue(m).set(n, BigDecimal.ZERO)
-            if (n == 0) u.getValue(m).put(n, BigDecimal.valueOf(1 / ((1 + m.toDouble().pow(2)))))
-            if (m == 0) u.getValue(m).put(n, BigDecimal.valueOf(Math.exp(n.toDouble())))
+            if (n == 0) u.getValue(m).put(n, BigDecimal.valueOf((1 / ((1 + m.toDouble().pow(2))))))
+            if (m == 0) u.getValue(m).put(n, BigDecimal.valueOf((Math.exp(n.toDouble()))))
         }
     }
     return u
